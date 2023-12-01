1267478dafb9fd882d959ea46fcf621eb3d93ed3
Todd Griffin <todd.griffin61@outlook.com>
2023-12-01 16:09:10 +0000

    completed part 1

--- 2023/day 1/answer.ts
+++ 2023/day 1/answer.ts
@@ -2,15 +2,32 @@ import fs from 'fs'
 
 const inputs: { [key: string]: string | undefined } = {};
 
-fs.readFile('/home/todd/Documents/advent-of-code/2023/day 1/input.txt', (err, data) => {
+const wordToNumber = {
+	one: '1',
+	two: '2',
+	three: '3',
+	four: '4',
+	five: '5',
+	six: '6',
+	seven: '7',
+	eight: '8',
+	nine: '9'
+}
+
+fs.readFile('/home/todd/Documents/advent-of-code/2023/day 1/testInput.txt', (err, data) => {
 	if (err) throw err;
 	inputs.test = data.toString()
 	const answerTry = calibrateDocument(inputs.test)
 	const answerCorrect = 142
 
-		console.log(`Success! The answer is ${answerTry}`)
+	console.log(`Your answer is${answerTry}\nThe expected answer is ${answerCorrect}`)
 })
 
+function stringOfNumbers(calibrationLine: string): string {
+	const numberString = calibrationLine.match(/one|two|three|four|five|six|seven|eight|nine|[0-9]/)
+	console.log(numberString)
+	return ''
+}
 
 function calibrateDocument(document: string): number {
 	const documentLines = document.split(/\r?\n/)
@@ -22,12 +39,12 @@ function calibrateDocument(document: string): number {
 				numbers.push(char)
 			}
 		}
-		return `${numbers[0]}${numbers[numbers.length-1]}`;
+		return `${numbers[0]}${numbers[numbers.length - 1]}`;
 	})
 
 	console.log(calibrationValues)
 
-	return calibrationValues.reduce((accumulator, currentValue) => 
+	return calibrationValues.reduce((accumulator, currentValue) =>
 		accumulator + Number(currentValue), 0
 	)
 }
