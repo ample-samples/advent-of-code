39e26fe1e7c34a0f5b4299e1618cdc85f5bd35ed
Todd Griffin <todd.griffin61@outlook.com>
2023-12-04 10:37:59 +0000

    completed part 1

--- 2023/day 4/part1.ts
+++ 2023/day 4/part1.ts
@@ -10,40 +10,33 @@ fs.readFile('/home/todd/Documents/advent-of-code/2023/day 4/input.txt', (err, da
 	const cards: string[][][] = [];
 	for (const element of inputs) {
 		const cardLeftNums = element
-		.slice(element.indexOf(':') + 1)
-		.split('|')[0]
-		.split(/\ +/)
-		.filter((x) => x !== '');
+			.slice(element.indexOf(':') + 1)
+			.split('|')[0]
+			.split(/\ +/)
+			.filter((x) => x !== '');
 
 		const cardRightNums = element
-		.slice(element.indexOf(':') + 1)
-		.split('|')[1]
-		.split(/\ +/)
-		.filter((x) => x !== '');
+			.slice(element.indexOf(':') + 1)
+			.split('|')[1]
+			.split(/\ +/)
+			.filter((x) => x !== '');
 
 		cards.push([cardLeftNums, cardRightNums]);
 	}
 
-	console.log(cards);
-
 	let sum = 0;
 	for (const card of cards) {
-		console.log(card);
 		let winningNumCount = 0;
 		for (const leftNum of card[0]) {
 			for (const rightNum of card[1]) {
 				if (leftNum === rightNum) {
-					console.log(leftNum, rightNum);
-					console.log("=== match ===");
 					winningNumCount++
 				}
 			}
 		}
 		if (winningNumCount === 0) {
-			console.log("This card is worth", 0, "points");
 			sum += 0
 		} else {
-			console.log("This card is worth", 2 ** (winningNumCount - 1), "points");
 			sum += 2 ** (winningNumCount - 1)
 		}
 	}
