157de51adfe1aff9c7300f3e2ab488b2a5e17114
Todd Griffin <todd.griffin61@outlook.com>
2023-12-04 10:25:03 +0000

    formatted cards

--- 2023/day 4/part1.ts
+++ 2023/day 4/part1.ts
@@ -4,22 +4,37 @@ fs.readFile('/home/todd/Documents/advent-of-code/2023/day 4/testInput.txt', (err
 	if (err) throw err;
 	const inputs = data
 		.toString()
-		.split('\n')
-	inputs.pop()
-	console.log(inputs)
+		.split('\n');
+	inputs.pop();
+	console.log(inputs);
 
-	const cards: string[][][] = []
+	const cards: string[][][] = [];
 	for (const element of inputs) {
-		const cardLeftNums = element.slice(element.indexOf(':') + 1).split('|')[0].split(/\ +/)
-		cardLeftNums.pop()
-		cardLeftNums.shift()
+		const cardLeftNums = element.slice(element.indexOf(':') + 1).split('|')[0].split(/\ +/);
+		cardLeftNums.pop();
+		cardLeftNums.shift();
 
-		const cardRightNums = element.slice(element.indexOf(':') + 1).split('|')[1].split(/\ +/)
-		cardRightNums.pop()
-		cardRightNums.shift()
+		const cardRightNums = element.slice(element.indexOf(':') + 1).split('|')[1].split(/\ +/);
+		cardRightNums.pop();
+		cardRightNums.shift();
 
-		cards.push([ cardLeftNums, cardRightNums ])
+		cards.push([ cardLeftNums, cardRightNums ]);
 	}
 
-	console.log(cards)
+	console.log(cards);
+
+	let sum = 0;
+	for (const card of cards) {
+		let winningNumCount = 0;
+		for (const leftNum of card[0]) {
+			for (const rightNum of card[1]) {
+				if (leftNum === rightNum) {
+					winningNumCount++
+				}
+			}
+		}
+		sum += 2 ** winningNumCount
+	}
+
+	console.log(sum)
 })
