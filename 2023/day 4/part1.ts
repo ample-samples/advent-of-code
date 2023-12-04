ee441cc83112c7a6748cd5d2fd12501654f48743
Todd Griffin <todd.griffin61@outlook.com>
2023-12-04 10:20:38 +0000

    formatted cards

--- 2023/day 4/part1.ts
+++ 2023/day 4/part1.ts
@@ -8,14 +8,18 @@ fs.readFile('/home/todd/Documents/advent-of-code/2023/day 4/testInput.txt', (err
 	inputs.pop()
 	console.log(inputs)
 
-	const cards: string[][] = []
+	const cards: string[][][] = []
 	for (const element of inputs) {
-		const cardWinningNums = element.slice(element.indexOf(':') + 1).split('|')[0].split(" ")
-		cards.push(cardWinningNums)
+		const cardLeftNums = element.slice(element.indexOf(':') + 1).split('|')[0].split(/\ +/)
+		cardLeftNums.pop()
+		cardLeftNums.shift()
 
+		const cardRightNums = element.slice(element.indexOf(':') + 1).split('|')[1].split(/\ +/)
+		cardRightNums.pop()
+		cardRightNums.shift()
+
+		cards.push([ cardLeftNums, cardRightNums ])
 	}
 
-	for (const card of cards) {
-	}
 	console.log(cards)
 })
